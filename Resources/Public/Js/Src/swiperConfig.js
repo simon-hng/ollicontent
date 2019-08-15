888845429609b02645a8005a10fe839bae7277a3
Simon Huang <simon.huang@outlook.de>
2019-08-15 14:29:57 +0200

    feat: add swiper settings

--- Resources/Public/Js/Src/swiperConfig.js
+++
@@ -1,9 +0,0 @@
-var swiper = new Swiper('.swiper-container', {
-    pagination: {
-        el: '.swiper-pagination',
-    },
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-});
